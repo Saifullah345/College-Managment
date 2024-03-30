f33e557a5f1c53fe9a691ef61f74299d428f5740
Danish khan <danishyazdani063@gmail.com>
2024-03-30 23:54:33 +0500

    college

--- frontend/src/pages/Homepage.js
+++ frontend/src/pages/Homepage.js
@@ -17,7 +17,7 @@ const Homepage = () => {
                         <StyledTitle>
                             Welcome to
                             <br />
-                            School Management
+                            College Management
                             <br />
                             System
                         </StyledTitle>
