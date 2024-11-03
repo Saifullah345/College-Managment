8a0b3e5ee80849050c80541ddab22a0f5888477f
sirajkhan8868 <sirajkhan8868447@gmail.com>
2024-11-03 02:43:08 -0800

    added

--- frontend/src/pages/admin/studentRelated/StudentDetail/index.jsx
+++ frontend/src/pages/admin/studentRelated/StudentDetail/index.jsx
@@ -97,7 +97,7 @@ const StudentDetail = () => {
           variant="h6"
           color="inherit"
           noWrap
-          // sx={{ flexGrow: 1 }}
+        // sx={{ flexGrow: 1 }}
         >
           Student Detail
         </Typography>
@@ -115,91 +115,104 @@ const StudentDetail = () => {
         "Loading..."
       ) : (
         <Grid container spacing={2}>
-          <Grid item lg={6} xs={12} columnGap={10} rowGap={3}>
+          <Grid item lg={4} xs={15} columnGap={10} rowGap={3}>
             <Box
               display={"flex"}
               flexDirection={"column"}
+              backgroundColor="#1976D3"
               border={"1px solid #ccc"}
               borderRadius={"10px"}
-              padding={3}
+              padding={5}
               boxShadow={1}
               marginTop={"20px"}
             >
-              <img
-                src={"https://eskooly.com/bb/uploads/students/no-image.png"}
-                height={100}
-                style={{ borderRadius: "100%" }}
-                width={100}
-                alt="reload"
-              />
+              <Box display={"flex"} justifyContent={"center"} alignItems={"center"} marginBottom={2}>
+                <img
+                  src={"https://eskooly.com/bb/uploads/students/no-image.png"}
+                  height={100}
+                  width={100}
+                  style={{ borderRadius: "100%" }}
+                  alt="reload"
+                />
+              </Box>
+              <h1 style={{ textAlign: 'center', color: '#fff' }}>Sadia Naz</h1>
+              <p style={{ textAlign: 'center', color: '#fff' }}>F20-0255</p>
+
               <Box
                 display={"flex"}
                 justifyContent={"space-between"}
                 gap={3}
                 marginTop={"5px"}
               >
-                <p>Name </p>
-                <h4>{data.name}</h4>
-              </Box>{" "}
-              <Box display={"flex"} justifyContent={"space-between"} gap={3}>
-                <p>Registration No</p>
-                <h4>{data.enrollmentNo}</h4>
               </Box>
-              <Box display={"flex"} justifyContent={"space-between"} gap={3}>
-                <p>Mobile No</p>
-                <h4>{data.mobileNumber}</h4>
+              <Box display={"flex"} justifyContent={"space-between"} style={{ paddingBottom: '8px' }} gap={3}>
+                <p style={{ color: '#FFFFFF' }}>Mobile No</p>
+                <Box display={"flex"} justifyContent={"space-between"} gap={3}>
+                  <p style={{ color: '#FFFFFF' }}>03412334445</p>
+                </Box>
               </Box>
-              <Box display={"flex"} justifyContent={"space-between"} gap={3}>
-                <p>Father Name</p>
-                <h4>{data.fatherName}</h4>
+
+              <Box display={"flex"} justifyContent={"space-between"} style={{ paddingBottom: '8px' }} gap={3}>
+                <p style={{ color: '#FFFFFF' }}>Father Name</p>
+                <Box display={"flex"} justifyContent={"space-between"} gap={3}>
+                  <p style={{ color: '#FFFFFF' }}>Sanjay</p>
+                </Box>
               </Box>
-              <Box display={"flex"} justifyContent={"space-between"} gap={3}>
-                <p>CNIC</p>
-                <h4>{data.cnic}</h4>
+
+              <Box display={"flex"} justifyContent={"space-between"} style={{ paddingBottom: '8px' }} gap={3}>
+                <p style={{ color: '#FFFFFF' }}>CNIC</p>
+                <Box display={"flex"} justifyContent={"space-between"} gap={3}>
+                  <p style={{ color: '#FFFFFF' }}>12345-678901-2</p>
+                </Box>
               </Box>
-              <Box display={"flex"} justifyContent={"space-between"} gap={3}>
-                <p>Date of Admission</p>
-                <h4>N/A</h4>
+
+              <Box display={"flex"} justifyContent={"space-between"} style={{ paddingBottom: '8px' }} gap={3}>
+                <p style={{ color: '#FFFFFF' }}>Date of Admission</p>
+                <Box display={"flex"} justifyContent={"space-between"} gap={3}>
+                  <p style={{ color: '#FFFFFF' }}>12/6/2022</p>
+                </Box>
               </Box>
-              <Box display={"flex"} justifyContent={"space-between"} gap={3}>
-                <p>Class</p>
-                <h4>{data?.sclassName?.sclassName}</h4>
+
+              <Box display={"flex"} justifyContent={"space-between"} style={{ paddingBottom: '8px' }} gap={3}>
+                <p style={{ color: '#FFFFFF' }}>DOS</p>
+                <Box display={"flex"} justifyContent={"space-between"} gap={3}>
+                  <p style={{ color: '#FFFFFF' }}>8/6/2002</p>
+                </Box>
               </Box>
-              <Box display={"flex"} justifyContent={"space-between"} gap={3}>
-                <p>DOB</p>
-                <h4>{data.dob}</h4>
+
+              <Box display={"flex"} justifyContent={"space-between"} style={{ paddingBottom: '8px' }} gap={3}>
+                <p style={{ color: '#FFFFFF' }}>Discount Free</p>
+                <Box display={"flex"} justifyContent={"space-between"} gap={3}>
+                  <p style={{ color: '#FFFFFF' }}>1234</p>
+                </Box>
               </Box>
-              <Box display={"flex"} justifyContent={"space-between"} gap={3}>
-                <p>Discount Fee</p>
-                <h4>{data.discountFee}</h4>
+
+              <Box display={"flex"} justifyContent={"space-between"} style={{ paddingBottom: '8px' }} gap={3}>
+                <p style={{ color: '#FFFFFF' }}>Gender</p>
+                <Box display={"flex"} justifyContent={"space-between"} gap={3}>
+                  <p style={{ color: '#FFFFFF' }}>Female</p>
+                </Box>
               </Box>
-              <Box display={"flex"} justifyContent={"space-between"} gap={3}>
-                <p>Gender</p>
-                <h4>{data.gender}</h4>
+
+              <Box display={"flex"} justifyContent={"space-between"} style={{ paddingBottom: '8px' }} gap={3}>
+                <p style={{ color: '#FFFFFF' }}>Province</p>
+                <Box display={"flex"} justifyContent={"space-between"} gap={3}>
+                  <p style={{ color: '#FFFFFF' }}>KpK</p>
+                </Box>
               </Box>
-              <Box display={"flex"} justifyContent={"space-between"} gap={3}>
-                <p>Religion</p>
-                <h4>{data.religion}</h4>
+
+              <Box display={"flex"} justifyContent={"space-between"} style={{ paddingBottom: '8px' }} gap={3}>
+                <p style={{ color: '#FFFFFF' }}>Session</p>
+                <Box display={"flex"} justifyContent={"space-between"} gap={3}>
+                  <p style={{ color: '#FFFFFF' }}>2024</p>
+                </Box>
               </Box>
-              <Box display={"flex"} justifyContent={"space-between"} gap={3}>
-                <p>Board</p>
-                <h4>{data.board}</h4>
-              </Box>
-              <Box display={"flex"} justifyContent={"space-between"} gap={3}>
-                <p>Session</p>
-                <h4>{data.session?.session}</h4>
-              </Box>
-              <Box display={"flex"} justifyContent={"space-between"} gap={3}>
-                <p>Province</p>
-                <h4>{data.provinces}</h4>
-              </Box>
-              <Box display={"flex"} justifyContent={"space-between"} gap={3}>
-                <p>District</p>
-                <h4>{data.district}</h4>
-              </Box>
-              <Box display={"flex"} justifyContent={"space-between"} gap={3}>
-                <p>Tehsil</p>
-                <h4>{data.tehsil}</h4>
+
+              <Box display={"flex"} justifyContent={"space-between"} style={{ paddingBottom: '8px' }} gap={3}>
+                <p style={{ color: '#FFFFFF' }}>Board</p>
+                <Box display={"flex"} justifyContent={"space-between"} gap={3}>
+                  <p style={{ color: '#FFFFFF' }}>Abbatabad Board</p>
+                </Box>
               </Box>
             </Box>
           </Grid>
@@ -213,42 +226,61 @@ const StudentDetail = () => {
               boxShadow={1}
               marginTop={"20px"}
             >
-              <h4>Attachment</h4>
-              <Box display={"flex"} gap={3}>
-                <p>ID Card Front Photo</p>
-                <a
-                  style={{ color: "blue" }}
-                  target="_blank"
-                  href={data.idCardFront}
-                  rel="noreferrer"
-                >
-                  {data.idCardFront?.slice(0, 30)}
-                </a>
+              <Box textAlign={"center"} gap={3}>
+                <h1 style={{ color: '#1976D3' }}>Upload ID card Front Image</h1>
+                <p>PNG,JPG,GIF up to 5 MB</p>
+                <button style={{ padding: '7px 12px', background: '#1976D3', color: '#fff', border: 'none' }}>Upload Image</button>
+
               </Box>
-              <Box display={"flex"} gap={3}>
-                <p>ID Card Back Photo</p>
-                <a
-                  style={{ color: "blue" }}
-                  target="_blank"
-                  href={data.idCardBack}
-                  rel="noreferrer"
-                >
-                  {data.idCardBack?.slice(0, 30)}
-                </a>
-              </Box>
-              <Box display={"flex"} gap={3}>
-                <p>Metric Certificate</p>
-                <a
-                  style={{ color: "blue" }}
-                  target="_blank"
-                  href={data.MetricDMC}
-                  rel="noreferrer"
-                >
-                  {data.MetricDMC?.slice(0, 30)}
-                </a>
+
+            </Box>
+            <Box
+              display={"flex"}
+              flexDirection={"column"}
+              border={"1px solid #ccc"}
+              borderRadius={"10px"}
+              padding={3}
+              boxShadow={1}
+              marginTop={"20px"}
+            >
+
+              <Box
+                display={"flex"}
+                flexDirection={"column"}
+                border={"1px solid #ccc"}
+                borderRadius={"10px"}
+                padding={3}
+                boxShadow={1}
+                marginTop={"20px"}
+              >
+                <Box textAlign={"center"} gap={3}>
+                  <h1 style={{ color: '#1976D3' }}>Upload ID card Front Image</h1>
+                  <p>PNG,JPG,GIF up to 5 MB</p>
+                  <button style={{ padding: '7px 12px', background: '#1976D3', color: '#fff', border: 'none' }}>Upload Image</button>
+
+                </Box>
               </Box>
             </Box>
+
+            <Box
+              display={"flex"}
+              flexDirection={"column"}
+              border={"1px solid #ccc"}
+              borderRadius={"10px"}
+              padding={3}
+              boxShadow={1}
+              marginTop={"20px"}
+            >
+              <Box textAlign={"center"} padding={'10px'} gap={3}>
+                <h1 style={{ color: '#1976D3' }}>Upload ID card Front Image</h1>
+                <p>PNG,JPG,GIF up to 5 MB</p>
+                <button style={{ padding: '7px 12px', background: '#1976D3', color: '#fff', border: 'none' }}>Upload Image</button>
+
+              </Box>
+            </Box>
+
           </Grid>
+
         </Grid>
       )}
     </div>
